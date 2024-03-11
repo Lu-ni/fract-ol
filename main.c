b4159e7fcf3724feb0add61e589cb7525cd5ddc3
lnicolli <lnicolli@c1r11s5.42lausanne.ch>
2024-03-11 14:53:33 +0100

    change windows name to fract-ol

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ void init(t_all *all, int argc, char **argv)
 	all->set.y_step = (all->set.y_max - all->set.y_min) / (all->args.high - 1.0);
 
 	all->vars.mlx = mlx_init();
-	all->vars.win = mlx_new_window(all->vars.mlx, all->args.high, all->args.width, "Hello world!");
+	all->vars.win = mlx_new_window(all->vars.mlx, all->args.high, all->args.width, "fract-ol");
 	all->img.img = mlx_new_image(all->vars.mlx, all->args.high, all->args.width);
 	all->img.addr = mlx_get_data_addr(all->img.img, &all->img.bits_per_pixel,
 	                                  &all->img.line_length, &all->img.endian);
